cc3e7bb6404ecb7e41b7be2d72071a555a235158
Diego Escarcega <143562608+clancytheman@users.noreply.github.com>
2025-02-04 16:30:15 -0600

    Add files via upload

---
+++ EVA1_4_COMPARAR_CADENAS.java
@@ -0,0 +1,17 @@
+package com.mycompany.eva1_4_comparar_cadenas;
+
+/**
+ *
+ * @author Diego Escarcega // 31/01/25
+ */
+public class EVA1_4_COMPARAR_CADENAS {
+
+    public static void main(String[] args) {
+        String cha1n = "Hello!";
+        String cha2n = "Hello!";
+        if (cha1n.equals(cha2n))
+            System.out.println("Same chains.");
+        else
+            System.out.println("Different chains.");
+    }
+}
